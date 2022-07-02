2cbe3540a21cc1af520f7ab4289a92b9265d0f94
Anurag <anuragvastav4992@gmail.com>
2022-07-02 18:05:01 +0530

    Project complete

---
+++ config/keys.js
@@ -0,0 +1,9 @@
+let port=process.env.PORT
+if(port == null || port == ""){
+    port=3000
+}
+
+module.exports={
+    mongoURI : ("mongodb+srv://anurag4992:Anhourlat6@cluster0.j80jq.mongodb.net/userDB"),
+    PORT : port
+}
